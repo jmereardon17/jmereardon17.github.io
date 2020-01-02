2a5c535100e27cf332999cfd2ab53102c17255e8
Jamie Reardon <decor1878@gmail.com>
2020-01-02 21:03:32 +0000

    Add project js files

--- js/data.js
+++ js/data.js
@@ -1,8 +1,9 @@
 const projects = [
   {
-    name: 'My Personal Profile Page',
+    name: 'Personal Profile Page',
     type: 'Techdegree',
-    desc: 'HTML, CSS.',
+    info: 'Customize a personal online profile by adding graphics, customizing text, and improving the look of a web page using Cascading Style Sheets.',
+    skills: 'HTML, CSS.',
     image: 'project-1.png',
     github: 'https://github.com/jmereardon17/techdegree-project-1',
     liveView: false
@@ -10,15 +11,17 @@ const projects = [
   {
     name: 'Responsive Layout',
     type: 'Techdegree',
-    desc: 'HTML, CSS, CSS Floats, Mobile-first approach.',
+    info: 'Create a mobile-first webpage layout that adjusts to fit standard devices using HTML, CSS, and responsive design.',
+    skills: 'HTML, CSS Floats, Mobile-first.',
     image: 'project-2.png',
     github: 'https://github.com/jmereardon17/techdegree-project-2',
     liveView: false
   },
   {
-    name: 'An Online Registration Form',
+    name: 'Online Registration Form',
     type: 'Techdegree',
-    desc: 'HTML, CSS, CSS Flexbox, Mobile-first approach',
+    info: 'Build a responsive, mobile-first registration form using a variety of HTML form elements.',
+    skills: 'HTML, CSS Flexbox, Mobile-first.',
     image: 'project-3.png',
     github: 'https://github.com/jmereardon17/techdegree-project-3',
     liveView: false
@@ -26,7 +29,8 @@ const projects = [
   {
     name: 'Web Style Guide',
     type: 'Techdegree',
-    desc: 'Sass, SCSS, CSS, Mobile-first approach.',
+    info: 'Create a style guide that can act as a personal and custom Bootstrap that can be dropped into any project to speed up development in general.',
+    skills: 'Sass, SCSS, Mobile-first.',
     image: 'project-4.png',
     github: 'https://github.com/jmereardon17/techdegree-project-4',
     liveView: false
@@ -34,7 +38,8 @@ const projects = [
   {
     name: 'HSolutions',
     type: 'Freelance',
-    desc: 'Responsive business webpage using HTML, CSS, Bootstrap, and the Mobile-first approach. Fully designed and developed.',
+    info: 'Responsive business webpage, fully designed and developed on the fly.',
+    skills: 'HTML, CSS, Bootstrap, Mobile-first.',
     image: 'project-5.png',
     github: 'https://github.com/jmereardon17/hsolutions-responsive-business-page',
     liveView: 'http://hsolutions.uk'
@@ -42,7 +47,8 @@ const projects = [
   {
     name: 'Interactive Photo Gallery',
     type: 'Techdegree',
-    desc: 'HTML, SCSS, CSS, JavaScript with custom search filter, jQuery plugin and customized jQuery animations.',
+    info: 'Create an interactive, searchable gallery of photos using JavaScript.',
+    skills: 'HTML, SCSS, JavaScript, jQuery plugin.',
     image: 'project-6.png',
     github: 'https://github.com/jmereardon17/techdegree-project-5',
     liveView: false
@@ -50,7 +56,8 @@ const projects = [
   {
     name: 'Game Show App',
     type: 'Techdegree',
-    desc: 'JavaScript, jQuery, CSS.',
+    info: 'Create a browser word guessing game using JavaScript to randomize a phrase that players will try to guess.',
+    skills: 'JavaScript, jQuery, CSS.',
     image: 'project-7.png',
     github: 'https://github.com/jmereardon17/techdegree-project-6',
     liveView: false
@@ -58,9 +65,33 @@ const projects = [
   {
     name: 'WebApp Dashboard',
     type: 'Techdegree',
-    desc: 'JavaScript, jQuery, SVG, Sass, SCSS, CSS Grid, Mobile-first approach.',
+    info: 'Build an interactive dashboard for a web application using SVG Graphics.',
+    skills: 'JavaScript, jQuery, SVG, Sass, SCSS, CSS Grid, Mobile-first.',
     image: 'project-8.png',
     github: 'https://github.com/jmereardon17/techdegree-project-7',
     liveView: false
+  },
+  {
+    name: 'Employee Directory',
+    type: 'Techdegree',
+    info: 'Create an employee directory by communicating with a third-party API.',
+    skills: 'JavaScript, Sass, SCSS, CSS Grid, CSS Flexbox, Mobile-first.',
+    image: 'project-9.png',
+    github: 'https://github.com/jmereardon17/techdegree-project-8',
+    liveView: false
   }
 ];
+
+const intro = `<p>Hi there! Thanks for visting my profile, I'm a Front-End Web Developer from Liverpool who loves technology. I have been a web developer since 2018, where I studied a techdegree with <a class="link" href="https://teamtreehouse.com" target="_blank">Treehouse</a>. Click the arrow below to find out more about me.</p>`;
+
+const about = `
+  <p>I’m a tech enthusiast living in England, Liverpool. I spend my days learning and coding in Front End development for the web. I got my hands on my first computer when I was 13 where I spent most of my time on the internet, socializing with people from all over the world.</p>
+
+  <p>Because of my strong interest in computing, I started to gain an interest in web development. I was very fascinated by how a webpage was built. From then on, I started to self teach myself how to code websites.</p>
+
+  <p>After having basic understanding concepts on how to code a webpage, I felt that the content that was available at the time wasn't enough for me to fully understand and grasp the concepts. I moved away from web development altogether and put my focus elsewhere.</p>
+
+  <p>Although this happened, occasionally something kept pulling me back to it and I would code webpages again. In 2018 I found <a class="link" href="https://teamtreehouse.com" target="_blank">Treehouse</a> where I picked up the Front End Web Development techdegree which I completed in 2020.</p>
+
+  <p>In my spare time, keeping pro-active and productive is a keen action I like to take, therefore I regularly enjoy keeping fit. I love and have a very strong interest in traveling which I like to go site-seeing around the world.</p>
+  `;
--- js/main.js
+++ js/main.js
@@ -1,49 +1,81 @@
+const nav = document.querySelector('nav');
+const mobileNavIcon = document.querySelector('.nav__item--hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+const headline = document.querySelector('h1');
+const tagline = document.querySelector('h2');
+const headerButton = document.querySelector('.btn--work');
+const aboutLink = document.getElementById('aboutLink');
+const portfolioLink = document.getElementById('portfolioLink');
+const contactLink = document.getElementById('contactLink');
+const main = document.querySelector('main');
+const aboutSection = document.getElementById('about');
+const arrowRight = document.querySelector('.arrow--right');
+const arrowLeft = document.querySelector('.arrow--left');
+const aboutCard = document.querySelector('.about-card');
+const recentWorkList = document.querySelector('.recent-work');
+const projectOverlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const portfolioButton = document.querySelector('.btn--all-projects');
 const portfolioList = document.querySelector('.portfolio');
+const skills = document.querySelectorAll('.skills__item');
 let latestProjects = [];
 
-const displayPortfolio = (portfolio, items) => {
-  for (let i = 0; i < items; i += 1) {
+/* ==============================================
+  HELPER FUNCTIONS
+================================================ */
+
+const displayPortfolio = () => {
+  for (let i = 0; i < projects.length; i += 1) {
     const li = document.createElement('li');
     li.className = 'portfolio__item';
     const container = document.createElement('div');
-    container.className = 'portfolio__container';
-    const portfolioImage = document.createElement('div');
-    portfolioImage.className = `portfolio__img`;
-    portfolioImage.style.backgroundImage = `url('../img/portfolio/${portfolio[i].image}')`;
-    portfolioImage.style.backgroundRepeat = 'no-repeat';
-    const overlay = document.createElement('div');
-    overlay.className = 'portfolio__overlay';
-    const h3 = document.createElement('h3');
-    h3.className = 'portfolio__name';
-    h3.textContent = portfolio[i].name;
-    const h4 = document.createElement('h4');
-    h4.className = 'portfolio__type';
-    h4.textContent = portfolio[i].type + ' Project';
-    const p = document.createElement('p');
-    p.className = 'portfolio__desc';
-    p.textContent = portfolio[i].desc;
-    const linksDiv = document.createElement('div');
-    linksDiv.style = 'width: 100%; margin-top: auto';
-    const gitLink = document.createElement('a');
-    gitLink.className = 'portfolio__link';
-    gitLink.href = portfolio[i].github;
-    gitLink.target = '_blank';
-    gitLink.textContent = 'View Details';
-    linksDiv.appendChild(gitLink);
-    if (portfolio[i].liveView !== false) {
+    container.className = 'project';
+    const projectImage = document.createElement('div');
+    projectImage.className = `project__image animated fadeIn`;
+    projectImage.style.backgroundImage = `url('../img/portfolio/${projects[i].image}')`;
+    projectImage.style.backgroundRepeat = 'no-repeat';
+    container.appendChild(projectImage);
+    const projectDetailsDiv = document.createElement('div');
+    projectDetailsDiv.className = 'project__overlay';
+    const projectName = document.createElement('h3');
+    projectName.className = 'project__name';
+    projectName.textContent = `${projects[i].name}`;
+    projectDetailsDiv.appendChild(projectName);
+    const projectType = document.createElement('h4');
+    projectType.className = 'project__type';
+    projectType.textContent = `${projects[i].type}`;
+    projectDetailsDiv.appendChild(projectType);
+    const projectInfo = document.createElement('p');
+    projectInfo.className = 'project__info';
+    projectInfo.textContent = `${projects[i].info}`;
+    projectDetailsDiv.appendChild(projectInfo);
+    const projectSkills = document.createElement('p');
+    projectSkills.className = 'project__skills';
+    projectSkills.textContent = `${projects[i].skills}`;
+    projectDetailsDiv.appendChild(projectSkills);
+    const projectLinks = document.createElement('div');
+    projectLinks.className = 'project__links';
+    const githubLink = document.createElement('a');
+    githubLink.className = 'btn project__link';
+    githubLink.textContent = 'GitHub';
+    githubLink.href = `${projects[i].github}`;
+    githubLink.target = '_blank';
+    projectLinks.appendChild(githubLink);
+    const viewFullLink = document.createElement('a');
+    viewFullLink.className = 'btn project__link';
+    viewFullLink.textContent = 'View Full';
+    projectLinks.appendChild(viewFullLink);
+    if (projects[i].liveView !== false) {
       const liveLink = document.createElement('a');
-      liveLink.className = 'portfolio__link';
-      liveLink.href = portfolio[i].liveView;
+      liveLink.className = 'btn project__link';
+      liveLink.textContent = 'Demo';
+      liveLink.href = `${projects[i].liveView}`;
       liveLink.target = '_blank';
-      liveLink.textContent = 'Live View';
-      linksDiv.appendChild(liveLink);
+      projectLinks.removeChild(viewFullLink);
+      projectLinks.appendChild(liveLink);
     }
-    overlay.appendChild(h3);
-    overlay.appendChild(h4);
-    overlay.appendChild(p);
-    overlay.appendChild(linksDiv);
-    container.appendChild(portfolioImage);
-    container.appendChild(overlay);
+    projectDetailsDiv.appendChild(projectLinks);
+    container.appendChild(projectDetailsDiv);
     li.appendChild(container);
     portfolioList.appendChild(li);
   }
@@ -52,8 +84,198 @@ const displayPortfolio = (portfolio, items) => {
 const getLatestProjects = arr => {
   latestProjects = arr.slice(Math.max(arr.length - 4, 1));
   latestProjects.reverse();
-  return latestProjects;
+
+  for (let i=0; i<latestProjects.length; i+=1) {
+    const li = document.createElement('li');
+    li.className = 'recent-work__item';
+    const projectDiv = document.createElement('div');
+    projectDiv.className = 'project';
+    const projectImage = document.createElement('div');
+    projectImage.className = 'project__image';
+    projectImage.style.backgroundImage = `url(../../../img/portfolio/${latestProjects[i].image})`;
+    const projectDetails = document.createElement('div');
+    projectDetails.className = 'project__details';
+    const projectName = document.createElement('h3');
+    projectName.className = 'project__name';
+    projectName.textContent = `${latestProjects[i].name}`;
+    projectDetails.appendChild(projectName);
+    const projectType = document.createElement('h4');
+    projectType.className = 'project__type';
+    projectType.textContent = `${latestProjects[i].type}`;
+    projectDetails.appendChild(projectType);
+    const projectInfo = document.createElement('p');
+    projectInfo.className = 'project__info';
+    projectInfo.textContent = `${latestProjects[i].info}`;
+    projectDetails.appendChild(projectInfo);
+    const projectSkills = document.createElement('p');
+    projectSkills.className = 'project__skills';
+    projectSkills.textContent = `${latestProjects[i].skills}`;
+    projectDetails.appendChild(projectSkills);
+    const projectLinks = document.createElement('div');
+    projectLinks.className = 'project__links';
+    const githubLink = document.createElement('a');
+    githubLink.className = 'btn project__link';
+    githubLink.textContent = 'GitHub';
+    githubLink.href = `${latestProjects[i].github}`;
+    githubLink.target = '_blank';
+    projectLinks.appendChild(githubLink);
+    if (latestProjects[i].liveView !== false) {
+      const liveLink = document.createElement('a');
+      liveLink.className = 'btn project__link';
+      liveLink.textContent = 'Demo';
+      liveLink.href = `${latestProjects[i].liveView}`;
+      liveLink.target = '_blank';
+      projectLinks.appendChild(liveLink);
+    }
+    projectDetails.appendChild(projectLinks);
+    projectDiv.appendChild(projectImage);
+    projectDiv.appendChild(projectDetails);
+    li.appendChild(projectDiv);
+    recentWorkList.appendChild(li);
+  }
 };
 
-// getLatestProjects(projects);
-// displayPortfolio(latestProjects, 4);
+const toggleElementStyles = scrollPos => {
+  if (scrollPos >= 95) {
+    headline.style.opacity = '0';
+  } else {
+    headline.style.opacity = '1';
+  }
+  if (scrollPos >= 155) {
+    tagline.style.opacity = '0';
+  } else {
+    tagline.style.opacity = '1';
+  }
+  if (scrollPos >= 265) {
+    headerButton.style.opacity = '0';
+  } else {
+    headerButton.style.opacity = '1';
+  }
+  if (scrollPos >= 550) {
+    nav.style.borderBottom = '6px solid #99b898';
+  } else {
+    nav.style.borderBottom = 'none';
+  }
+  if (scrollPos >= 2433) {
+    let delay = 0;
+    for (let i = 0; i < skills.length; i += 1) {
+      delay += 0.5;
+      skills[i].classList.add('fadeIn');
+      skills[i].style.animationDelay = `${delay}s`;
+    }
+  }
+}
+
+const toggleAbout = () => {
+  let title = aboutCard.querySelector('h3');
+  let content = aboutCard.querySelector('.about-card__content');
+  if (title.textContent === 'About Me') {
+    $(aboutCard).fadeOut(function(){
+      title.textContent = 'My Journey as a Web Developer';
+      content.innerHTML = `
+        <img src="../icons/macbook.svg" alt="MacBook SVG Icon" class="macbook">
+        <img src="../icons/html-icon.svg" alt="HTML SVG Icon" class="html-icon">
+        <img src="../icons/css-icon.svg" alt="CSS SVG Icon" class="css-icon">
+        <img src="../icons/js-icon.svg" alt="JS SVG Icon" class="js-icon">
+        ${about}
+      `;
+    });
+    arrowRight.style = 'transform: translateX(300vh); opacity: 0';
+    window.setTimeout( () => {
+      arrowRight.style.display = 'none';
+    }, 100);
+    arrowLeft.style = 'transform: translateX(0); opacity: 1';
+    $(arrowLeft).show();
+    $(aboutCard).fadeIn();
+  } else {
+    $(aboutCard).fadeOut(function(){
+      title.textContent = 'About Me';
+      content.innerHTML = intro;
+    });
+    arrowRight.style = 'transform: translateX(0); opacity: 1';
+    arrowLeft.style = 'transform: translateX(-300vh); opacity: 0';
+    $(aboutCard).fadeIn();
+  }
+}
+
+const displayOverlay = projectItem => {
+  modal.style.backgroundImage = `url(../../../img/portfolio/project-${projectItem}.png)`;
+  modal.innerHTML = '<span class="close-icon">&times;</span>'
+  projectOverlay.classList.remove('hidden');
+  projectOverlay.classList.remove('flipOutY');
+  projectOverlay.classList.add('flipInY');
+  const closeIcon = document.querySelector('.close-icon');
+  closeIcon.addEventListener('click', () => {
+    projectOverlay.classList.remove('flipInY');
+    projectOverlay.classList.add('flipOutY');
+    window.setTimeout( () => {
+      projectOverlay.classList.add('hidden');
+    },800);
+  });
+}
+
+const togglePortfolio = () => {
+  if (portfolioList.classList.contains('hidden')) {
+    portfolioList.classList.remove('hidden');
+    portfolioButton.textContent = 'Hide All';
+  } else {
+    portfolioList.classList.add('hidden');
+    portfolioButton.textContent = 'View All';
+  }
+}
+
+getLatestProjects(projects);
+displayPortfolio();
+
+/* ==============================================
+  EVENT LISTENERS
+================================================ */
+
+window.onload = () => {
+  const portfolioProjects = portfolioList.querySelectorAll('.project__image');
+  let delay = 0;
+  for (let i=0; i<portfolioProjects.length; i+=1) {
+    delay += .5;
+    portfolioProjects[i].style.animationDelay = `${delay}s`;
+  }
+  const allProjects = document.querySelectorAll('.project');
+  allProjects.forEach(item => {
+    item.addEventListener('click', e => {
+      let projectType = item.parentNode.parentNode;
+      let image = item.firstElementChild;
+      if (projectType.className === 'recent-work' || e.target.textContent === 'View Full') {
+        let imageURL = image.getAttribute('style');
+        let projectNo = parseInt(imageURL.match(/(\d+)/));
+        displayOverlay(projectNo);
+      }
+    });
+  });
+}
+
+window.addEventListener('scroll', () => {
+  let wS = window.scrollY;
+  toggleElementStyles(wS);
+});
+
+mobileNavIcon.addEventListener('click', () => {
+  if (mobileNav.classList.contains('hidden')) {
+    mobileNav.classList.remove('hidden');
+    mobileNav.classList.remove('fadeOutUp');
+  } else {
+    mobileNav.classList.add('fadeOutUp');
+    window.setTimeout( () => {
+      mobileNav.classList.add('hidden');
+    },400);
+  }
+});
+
+mobileNav.addEventListener('click', e => {
+  if (e.target.classList.contains('nav__link--mobile')) {
+    mobileNav.classList.add('hidden');
+  }
+});
+
+arrowRight.addEventListener('click', toggleAbout);
+arrowLeft.addEventListener('click', toggleAbout);
+
+portfolioButton.addEventListener('click', togglePortfolio);
