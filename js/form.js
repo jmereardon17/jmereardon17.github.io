21b25713ab3dc00b731b0d6f3009054257874e2a
Jamie Reardon <jamie.reardon91@gmail.com>
2020-08-27 22:34:02 +0100

    Update form handler

--- js/form.js
+++ js/form.js
@@ -1,10 +1,21 @@
-const serviceSubmit = document.querySelector('.service-form__btn');
+const serviceForm = document.querySelector('.service-form');
+const formValidationURL = 'http://hsolutions.uk/jamie/confirm.php'
 
-const formHandler = () => {
-  fetch('http://hsolutions.uk/jamie/confirm.php')
-    .then(response => JSON.parse(response))
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
+const formHandler = (e, form) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  fetch(formValidationURL, {
+    method: 'post',
+    body: formData
+  }).then(response => response.text())
+    .then(text => console.log(text))
+    .catch(error => console.error(error))
+
+  // fetch('http://hsolutions.uk/jamie/confirm.php')
+  //   .then(response => JSON.parse(response))
+  //   .then(data => console.log(data))
+  //   .catch(error => console.log(error))
 }
 
-serviceSubmit.addEventListener('click', formHandler);
+serviceForm.addEventListener('submit', (e) => formHandler(e, serviceForm));
